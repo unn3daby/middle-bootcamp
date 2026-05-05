44d0aa124cfaafd882dbe973321c21c4d799fdcc
Marchenko Ivan <91330648+unn3daby@users.noreply.github.com>
2026-05-05 12:40:07 +0300

    Update hw3.js

--- 2/hw3.js
+++ 2/hw3.js
@@ -13,7 +13,7 @@ const moveConfig = {
 const DEFAULT_MOVE_PERCENT = 120;
 
 export const clearXPosition = (target) =>
-  gsap.to(target, { xPercent: 0, ...clearConfig });
+	gsap.to(target, { xPercent: 0, ...clearConfig });
 
 export const clearYPosition = (target) =>
 	gsap.to(target, { yPercent: 0, ...clearConfig });
@@ -36,82 +36,82 @@ export const moveFromRight = (target, percent = DEFAULT_MOVE_PERCENT) =>
 // ===============================================
 
 function preventDefaultPositions() {
- 	  clearXPosition(document.querySelector(".cta"));
-		clearXPosition(document.querySelector(".img"));
-		clearYPosition(document.querySelector(".block"));
-		clearXPosition(document.querySelector(".grid"));
+	clearXPosition(document.querySelector(".cta"));
+	clearXPosition(document.querySelector(".img"));
+	clearYPosition(document.querySelector(".block"));
+	clearXPosition(document.querySelector(".grid"));
 }
 
 function handleDesktopAnimations(origin, direction, destination) {
-  if (destination.index == 0 && direction == "up") {
-    return preventDefaultPositions();
-  }
-
+	if (destination.index == 0 && direction == "up") {
+		return preventDefaultPositions();
+	}
+	
 	if (origin.index == 0 && direction == "down") {
 		moveTop(document.querySelector(".block"));
-    destination.item.classList.add("bg");
-    return;
-  }
-
-  if (origin.index == 1 && direction == "up") {
-		clearYPosition(document.querySelector(".block"));
-    origin.item.classList.remove("bg");
-    return;
+		destination.item.classList.add("bg");
+		return;
 	}
-
+	
+	if (origin.index == 1 && direction == "up") {
+		clearYPosition(document.querySelector(".block"));
+		origin.item.classList.remove("bg");
+		return;
+	}
+	
 	if (origin.index == 2 && direction == "up") {
-    destination.item.classList.add("bg");
-    return;
-  }
-
-  return;
+		destination.item.classList.add("bg");
+		return;
+	}
+	
+	return;
 }
 
 
 function handleMobileAnimations(origin, direction) {
- 	if (origin.index == 0 && direction == "down") {
+	if (origin.index == 0 && direction == "down") {
 		moveLeft(document.querySelector(".cta"));
-    moveRight(document.querySelector(".mg"));
-    return;
-  }
+		moveRight(document.querySelector(".mg"));
+		return;
+	}
 
-  if (origin.index == 1 && direction == "up") {
+	if (origin.index == 1 && direction == "up") {
 		clearXPosition(document.querySelector(".cta"));
-    clearXPosition(document.querySelector(".img"));
-    return;
-  }
+		clearXPosition(document.querySelector(".img"));
+		return;
+	}
 
- 	if (origin.index == 1 && direction == "down") {
-    moveRight(document.querySelector(".grid"));
-    return;
-  }
+	if (origin.index == 1 && direction == "down") {
+		moveRight(document.querySelector(".grid"));
+		return;
+	}
 
-  if (origin.index == 2 && direction == "up") {
-    clearXPosition(document.querySelector(".grid"));
-    return;
-  }
+	if (origin.index == 2 && direction == "up") {
+		clearXPosition(document.querySelector(".grid"));
+		return;
+	}
 
-  if (origin.index == 2 && direction == "down") {
+	if (origin.index == 2 && direction == "down") {
 		moveRight(document.querySelector(".container"));
-    moveRight(document.querySelector(".mobile-text"));
+		moveRight(document.querySelector(".mobile-text"));
 		return;
-  }
+	}
 
-  if (origin.index == 3 && direction == "up") {
-    clearXPosition(document.querySelector(".container"));
-    clearXPosition(document.querySelector(".mobile-text"));
+	if (origin.index == 3 && direction == "up") {
+		clearXPosition(document.querySelector(".container"));
+		clearXPosition(document.querySelector(".mobile-text"));
 		return;
-  }
-
+	}
+	
   return;
 }
 
 function init(origin, destination, direction) {
-  const isDesktopMedia = window.matchMedia("(min-width: 1200px)").matches;
-
-  if (isDesktopMedia) {
-    handleDesktopAnimations(origin, direction, destination)
-  }
-
-  handleMobileAnimations(origin, direction);
+	const isDesktopMedia = window.matchMedia("(min-width: 1200px)").matches;
+	
+	if (isDesktopMedia) {
+		handleDesktopAnimations(origin, direction, destination)
+	}
+	
+	handleMobileAnimations(origin, direction);
 }
