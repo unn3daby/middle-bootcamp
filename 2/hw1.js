9346d6a7edfbcd46fa999a8cfd72cca274b4046b
ilmarch1 <ilmarch1@mts.ru>
2026-05-05 11:30:54 +0300

    feat: formatting

--- 2/hw1.js
+++ 2/hw1.js
@@ -6,9 +6,9 @@ class DateProcessor {
 	}
 
 	static getDateArray(date) {
-  	const day = date.getDate().toString().padStart(2, "0");
-  	const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  	const year = date.getFullYear();
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear();
 
 	  return [day, month, year]
 	}
